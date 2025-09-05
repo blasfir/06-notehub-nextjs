7932a27f6bc00062f1a2f5c734cef7ecb6f3df65
Kate <Katezador-ipt27@lll.kpi.ua>
2025-09-05 12:13:09 +0300

    add edits

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,17 +1,18 @@
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
-import NoteDetailsClient from "./NoteDetails.client"
+import NoteDetailsClient from "./NoteDetails.client";
 
-type Props = {
-  params: { id: string };
-};
-
-export default async function NoteDetailsPage({ params }: Props) {
+export default async function NoteDetailsPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params; 
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ["note", params.id],
-    queryFn: () => fetchNoteById(params.id),
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
   });
 
   return (
@@ -20,3 +21,4 @@ export default async function NoteDetailsPage({ params }: Props) {
     </HydrationBoundary>
   );
 }
+
--- components/SearchBox/SearchBox.tsx
+++ components/SearchBox/SearchBox.tsx
@@ -1,5 +1,4 @@
-//import { useState, useEffect } from 'react';
-//import { useDebounce } from 'use-debounce';
+
 import css from './SearchBox.module.css';
 
 interface SearchBoxProps {
